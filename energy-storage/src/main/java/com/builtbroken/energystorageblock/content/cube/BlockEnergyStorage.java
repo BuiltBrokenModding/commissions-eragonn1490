dd31e2d65866d3d39d5b1de1fe2b42f224db4095
Robert WC Seifert <rseifert.phone@gmail.com>
2018-07-13 00:44:32 -0400

    Fixed items not dropping from battery box

--- energy-storage/src/main/java/com/builtbroken/energystorageblock/content/cube/BlockEnergyStorage.java
+++ energy-storage/src/main/java/com/builtbroken/energystorageblock/content/cube/BlockEnergyStorage.java
@@ -12,6 +12,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -21,6 +23,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
 
@@ -97,6 +100,29 @@ public class BlockEnergyStorage extends Block implements ITileEntityProvider
             }
         }
         return false;
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
+    {
+        TileEntity tileentity = worldIn.getTileEntity(pos);
+
+        if (tileentity instanceof TileEntityEnergyStorage)
+        {
+            ItemStackHandler handler = ((TileEntityEnergyStorage) tileentity).inventory;
+            for (int i = 0; i < handler.getSlots(); ++i)
+            {
+                ItemStack itemstack = handler.getStackInSlot(i);
+
+                if (!itemstack.isEmpty())
+                {
+                    InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), itemstack);
+                    handler.setStackInSlot(i, ItemStack.EMPTY);
+                }
+            }
+        }
+
+        super.breakBlock(worldIn, pos, state);
     }
 
     @Nullable
